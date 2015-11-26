270818fce204d92a1b2a35548874b326e3f86833
Pavel Kucherbaev <pavel.kucherbaev@gmail.com>
2015-11-26 10:32:36 +0100

    non static

--- src/introsde/document/endpoint/PeoplePublisher.java
+++ src/introsde/document/endpoint/PeoplePublisher.java
@@ -4,18 +4,18 @@ import introsde.document.ws.PeopleImpl;
 import javax.xml.ws.Endpoint;
 
 public class PeoplePublisher {
-    public static String SERVER_URL = "http://localhost";
-    public static String PORT = "6902";
-    public static String BASE_URL = "/ws/people";
+    public String SERVER_URL = "http://localhost";
+    public String PORT = "6902";
+    public String BASE_URL = "/ws/people";
 
-    public static String getEndpointURL() {
-	if (String.valueOf(System.getenv("PORT")) != "null"){
-            PORT=String.valueOf(System.getenv("PORT"));
+    public String getEndpointURL() {
+	    if (String.valueOf(System.getenv("PORT")) != "null"){
+            PORT = String.valueOf(System.getenv("PORT"));
         }
         return SERVER_URL+":"+PORT+BASE_URL;
     }
 
-    public static void main(String[] args) {
+    public void main(String[] args) {
         String endpointUrl = getEndpointURL();
         System.out.println("Starting People Service...");
         System.out.println("--> Published. Check out "+endpointUrl+"?wsdl");
