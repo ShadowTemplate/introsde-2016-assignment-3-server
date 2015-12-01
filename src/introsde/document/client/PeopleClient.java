66640ddde09f6801b8e53056a3d910dc4d81ddfd
Pavel Kucherbaev <pavel.kucherbaev@gmail.com>
2015-12-01 20:45:22 +0100

    added update example

--- src/introsde/document/client/PeopleClient.java
+++ src/introsde/document/client/PeopleClient.java
@@ -5,23 +5,29 @@ import introsde.document.ws.People;
 
 import java.net.URL;
 import java.util.List;
+import java.util.UUID;
 
 import javax.xml.namespace.QName;
 import javax.xml.ws.Service;
 
 public class PeopleClient{
     public static void main(String[] args) throws Exception {
-        URL url = new URL("http://localhost:6902/ws/people?wsdl");
+        URL url = new URL("http://10.196.210.219:6902/ws/people?wsdl");
         //1st argument service URI, refer to wsdl document above
         //2nd argument is service name, refer to wsdl document above
         QName qname = new QName("http://ws.document.introsde/", "PeopleService");
         Service service = Service.create(url, qname);
 
         People people = service.getPort(People.class);
+        // read person with id 1
         Person p = people.readPerson(1);
-        List<Person> pList = people.getPeople();
-        System.out.println("Result ==> "+p);
-        System.out.println("Result ==> "+pList);
-        System.out.println("First Person in the list ==> "+pList.get(0).getName());
+        System.out.println("First Person with id=1 ==> "+p.getName());
+        // change name of the person with id 1
+        String uuid = UUID.randomUUID().toString();
+        p.setName("Veabrechuk New name"+ uuid);
+        people.updatePerson(p);
+        // read again this person
+        p = people.readPerson(1);
+        System.out.println("First Person with id=1 ==> "+p.getName());
     }
 }
