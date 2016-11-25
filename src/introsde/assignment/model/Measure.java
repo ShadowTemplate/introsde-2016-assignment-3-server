e3688262a16ef563641f6f489b615a64efa373c8
ShadowTemplate <taneburgo+shadowtemplate@gmail.com>
2016-11-25 19:21:17 +0100

    Added getters/setters

--- src/introsde/assignment/model/Measure.java
+++ src/introsde/assignment/model/Measure.java
@@ -21,4 +21,44 @@ public class Measure {
     public Measure() {
         
     }
+
+    public Long getMid() {
+        return mid;
+    }
+
+    public void setMid(Long mid) {
+        this.mid = mid;
+    }
+
+    public Date getDateRegistered() {
+        return dateRegistered;
+    }
+
+    public void setDateRegistered(Date dateRegistered) {
+        this.dateRegistered = dateRegistered;
+    }
+
+    public String getMeasureType() {
+        return measureType;
+    }
+
+    public void setMeasureType(String measureType) {
+        this.measureType = measureType;
+    }
+
+    public String getMeasureValue() {
+        return measureValue;
+    }
+
+    public void setMeasureValue(String measureValue) {
+        this.measureValue = measureValue;
+    }
+
+    public String getMeasureValueType() {
+        return measureValueType;
+    }
+
+    public void setMeasureValueType(String measureValueType) {
+        this.measureValueType = measureValueType;
+    }
 }
--- src/introsde/assignment/model/Person.java
+++ src/introsde/assignment/model/Person.java
@@ -20,4 +20,44 @@ public class Person {
     public Person() {
 
     }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public List<Measure> getCurrentHealth() {
+        return currentHealth;
+    }
+
+    public void setCurrentHealth(List<Measure> currentHealth) {
+        this.currentHealth = currentHealth;
+    }
+
+    public List<Measure> getHealthHistory() {
+        return healthHistory;
+    }
+
+    public void setHealthHistory(List<Measure> healthHistory) {
+        this.healthHistory = healthHistory;
+    }
 }
